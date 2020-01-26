26733866dcd6349a619bd5441c36aaf057bf6459
oabanin-HOME <juristaba@gmail.com>
2020-01-26 23:33:22 +0300

    add flex-box example, cards, embed videos

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $('.carousel').carousel({
   interval: 2000,
   keyboard: false
-})
+});
 
 $('#exampleModal').modal({
   keyboard:false,
-  backdrop:false
-})
+  backdrop:false,
+  show:false
+});
+
+//$('#exampleModal').modal('hide');
